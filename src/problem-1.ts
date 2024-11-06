d9a5291f1c735ecb0c353a95bf511ec169926114
shahadathhs <shahadathhossensajib732@gmail.com>
2024-11-06 18:56:04 +0600

    feat: problem two solved

--- src/problem-1.ts
+++ src/problem-1.ts
@@ -1,13 +1,17 @@
 // * Function to return the sum of all numbers in an array
 const sumArray = (array: Array<number>): number => {
+  // * Step-1: Create a variable to store the sum
   let sum: number = 0;
+  // * Step-2: Loop through the array and add each element to the sum
   for (const element of array) {
     sum += element;
   }
+  // * Step-3: Return the sum
   return sum;
 }
 
-// * Call the function with an array of numbers
-const array: Array<number> = [1, 2, 3, 4, 5];
-const result: number = sumArray(array);
-console.log(result);
+// * Call the function with some array of numbers
+const arrayOfNumbers: Array<number> = [1, 2, 3, 4, 5];
+// const result: number = sumArray(arrayOfNumbers);
+// console.log(result);
+sumArray(arrayOfNumbers);
